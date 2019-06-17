825d8c6b98f4e92dc4cf28bf137ae02d79adfc31
nils2525 <nilslehb@gmail.com>
2019-06-17 22:15:59 +0200

    first scrapper implementation

---
+++ src/ScrappyClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebScrapper
+{
+    public class ScrappyClient : HttpClient
+    {
+        public int DelayFactor { get; set; } = 3;
+        public HttpClientHandler HttpClientHandler { get; set; }
+
+        private int _lastResponseTime;
+        private long _lastResponseTicks;
+
+        public ScrappyClient(HttpClientHandler handler) : base(handler)
+        {
+            _lastResponseTime = 0;
+            _lastResponseTicks = DateTime.Now.Ticks;
+        }
+
+        public override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            //Wait until delay is expired
+            while(_lastResponseTicks + (_lastResponseTime * DelayFactor) >= DateTime.Now.Ticks)
+            {
+                Task.Delay(1).Wait();
+            }
+
+            //ToDo if no user agent in property or request found, throw exception
+            if(request.Headers.UserAgent == null)
+            {
+                throw new Exception();
+            }
+
+            return base.SendAsync(request, cancellationToken);            
+        }
+    }
+}
